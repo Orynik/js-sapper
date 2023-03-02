18dbdb60a95add398a9615d88f8dafc3ae66bcdb
Orynik <dkrovel@gmail.com>
2023-03-03 01:32:32 +0800

    change arrays to global Map structure

--- js/index.js
+++ js/index.js
@@ -10,36 +10,33 @@ tileMap.set(6,'--six')
 tileMap.set(7,'--seven')
 tileMap.set(8,'--eight')
 
+
+let tilesState = new Map()
+
 // Консты для расчетов
 const tileWidth = 17
-const boardTaleCountY = 16
-const boardTaleCountX = 16
 const bombQty = 40
-const tillQty = boardTaleCountX * boardTaleCountY
-let board = document.getElementById('board')
-
-
+let boardSize = 16 * 16
+let boardSideLength = Math.sqrt(boardSize)
 
 // Рабочие переменные
 let isInitialClick = true
-let bombs = new Set()
-let numbers = []
-let boardSize = 16
-
-
+let board = document.getElementById('board')
 let isEndGame = false
 
 // Функции
 function renderTile(){
-	board.style.width = `${boardTaleCountY * tileWidth}px`
+	board.style.width = `${boardSideLength * tileWidth}px`
 
-	for(let index = 0; index < tillQty; index++){
+	for(let index = 0; index < boardSize; index++){
 		createTile(index)
 	}
 }
 
 function setup(initialTile){
 	let bombsLeft = bombQty
+	let numberBuff = {}
+	let bombsBuff = {}
 
 	let x = 0
 	let y = 0
@@ -47,21 +44,48 @@ function setup(initialTile){
 
 	//Вычисление позиций бомб
 	document.querySelectorAll('.tile').forEach((tile) => {
+		function addInNumberBuff(key) {
+			if(Object.hasOwn(numberBuff,key)){
+				numberBuff[key]++
+				return
+			}
+
+			numberBuff[key] = 1
+		}
+
 		tile.setAttribute('data-tile', `${x},${y}`)
-		let random_boolean = Math.random() < 0.2
+
+		let random_boolean = Math.random() < 0.25
+
 		if (tile !== initialTile && random_boolean && bombsLeft) {
 			bombsLeft--
-			bombs.add(`${x},${y}`)
-			if (x > 0) numbers.push(`${x-1},${y}`)
-			if (x < boardTaleCountX - 1) numbers.push(`${x+1},${y}`)
-			if (y > 0) numbers.push(`${x},${y-1}`)
-			if (y < boardTaleCountX - 1) numbers.push(`${x},${y+1}`)
+			bombsBuff[`${x},${y}`] = true
+
+			if (x > 0) {
+				addInNumberBuff(`${x-1},${y}`)
+			}		
+			if (x < boardSize - 1)	{
+				addInNumberBuff(`${x+1},${y}`)
+			}		
+			if (y > 0){
+				addInNumberBuff(`${x},${y-1}`)
+			} 
+			if (y < boardSideLength - 1) {
+				addInNumberBuff(`${x},${y+1}`)
+			}
 			
-			if (x > 0 && y > 0) numbers.push(`${x-1},${y-1}`)
-			if (x < boardTaleCountX - 1 && y < boardTaleCountX - 1) numbers.push(`${x+1},${y+1}`)
-			
-			if (y > 0 && x < boardTaleCountX - 1) numbers.push(`${x+1},${y-1}`)
-			if (x > 0 && y < boardTaleCountX - 1) numbers.push(`${x-1},${y+1}`)
+			if (x > 0 && y > 0) {
+				addInNumberBuff(`${x-1},${y-1}`)
+			}
+			if (x < boardSideLength - 1 && y < boardSideLength - 1){
+				addInNumberBuff(`${x+1},${y+1}`)
+			} 
+			if (y > 0 && x < boardSideLength - 1) 	{
+				addInNumberBuff(`${x+1},${y-1}`)
+			}
+			if (x > 0 && y < boardSideLength - 1){
+				addInNumberBuff(`${x-1},${y+1}`)
+			} 
 		}
 
 		x++
@@ -72,16 +96,12 @@ function setup(initialTile){
 		}
 	})
 
-	// TODO: Поменять на внутреннее хранилище вместо записи в data-num
-	numbers.forEach(num => {
-		let coords = num.split(',')
-		let tile = document.querySelectorAll(`[data-tile="${parseInt(coords[0])},${parseInt(coords[1])}"]`)[0]
-		let dataNum = parseInt(tile.getAttribute('data-num')) || 0
-
-		if(!bombs.has(num)){
-			tile.setAttribute('data-num', dataNum + 1)
-		}
-	})
+	Object.entries(numberBuff).forEach(([key,value]) => {
+		tilesState.set(key,{
+			count: value,
+			...(Object.hasOwn(bombsBuff,key) && {hasBomb: true}),
+		}) 
+	}) 
 }
 
 	
@@ -94,22 +114,22 @@ function clickTile(tile){
 	if (tile.classList.contains('tile--checked') || tile.classList.contains('tile--flagged')) return
 
 	let coordinate = tile.getAttribute('data-tile')
-	if (bombs.has(coordinate)) {
-		endGame(tile)
-		return
-	} else {
-		let num = tile.getAttribute('data-num')
-		if (num != null) {
+	
+	if(tilesState.has(coordinate)){
+		if (tilesState.get(coordinate).hasBomb) {
+			endGame(tile)
+		} else {
+			let num = tilesState.get(coordinate).count
 			tile.classList.add('tile--checked')
 			tile.classList.add(`tile${tileMap.get(+num)}`)
 			setTimeout(() => {
 				// checkVictory()
 			}, 100)
-			return
 		}
-		
-		checkTile(coordinate)
+		return
 	}
+
+	checkTile(coordinate)
 	tile.classList.add('tile--checked')
 }
 
@@ -129,7 +149,7 @@ function checkTile(coordinate){
 			let targetW = document.querySelectorAll(`[data-tile="${x-1},${y}"`)[0]
 			clickTile(targetW, `${x-1},${y}`)
 		}
-		if (x < 16 - 1) {
+		if (x < boardSideLength - 1) {
 			let targetE = document.querySelectorAll(`[data-tile="${x+1},${y}"`)[0]
 			clickTile(targetE, `${x+1},${y}`)
 		}
@@ -137,7 +157,7 @@ function checkTile(coordinate){
 			let targetN = document.querySelectorAll(`[data-tile="${x},${y-1}"]`)[0]
 			clickTile(targetN, `${x},${y-1}`)
 		}
-		if (y < 16 - 1) {
+		if (y < boardSideLength - 1) {
 			let targetS = document.querySelectorAll(`[data-tile="${x},${y+1}"]`)[0]
 			clickTile(targetS, `${x},${y+1}`)
 		}
@@ -146,16 +166,16 @@ function checkTile(coordinate){
 			let targetNW = document.querySelectorAll(`[data-tile="${x-1},${y-1}"`)[0]
 			clickTile(targetNW, `${x-1},${y-1}`)
 		}
-		if (x < 16 - 1 && y < 16 - 1) {
+		if (x < 16 - 1 && y < boardSideLength - 1) {
 			let targetSE = document.querySelectorAll(`[data-tile="${x+1},${y+1}"`)[0]
 			clickTile(targetSE, `${x+1},${y+1}`)
 		}
 		
-		if (y > 0 && x < 16 - 1) {
+		if (y > 0 && x < boardSideLength - 1) {
 			let targetNE = document.querySelectorAll(`[data-tile="${x+1},${y-1}"]`)[0]
 			clickTile(targetNE, `${x+1},${y-1}`)
 		}
-		if (x > 0 && y < 16 - 1) {
+		if (x > 0 && y < boardSideLength - 1) {
 			let targetSW = document.querySelectorAll(`[data-tile="${x-1},${y+1}"`)[0]
 			clickTile(targetSW, `${x-1},${y+1}`)
 		}
@@ -207,8 +227,9 @@ function endGame(tile){
 
 	tile.classList.add('tile-bomb--focus')
 
-	bombs.forEach((bombCoords) => {
-		document.querySelector(`.tile[data-tile="${bombCoords}"]`).classList.add('tile-bomb')
+	tilesState.forEach((value,key) =>{
+		if(!Object.hasOwn(value,'hasBomb')) return
+		document.querySelector(`.tile[data-tile="${key}"]`).classList.add('tile-bomb')
 	})
 }
 
--- public/js/index.js
+++ public/js/index.js
@@ -10,56 +10,33 @@ tileMap.set(6,'--six')
 tileMap.set(7,'--seven')
 tileMap.set(8,'--eight')
 
+
+let tilesState = new Map()
+
 // Консты для расчетов
 const tileWidth = 17
-const boardTaleCountY = 16
-const boardTaleCountX = 16
 const bombQty = 40
-const tillQty = boardTaleCountX * boardTaleCountY
-let board = document.getElementById('board')
-
-
+let boardSize = 16 * 16
+let boardSideLength = Math.sqrt(boardSize)
 
 // Рабочие переменные
-let currentTileId = null
 let isInitialClick = true
-let bombs = new Set()
-let numbers = []
-let boardSize = 16
-
-
-// Обработчики
-// board.addEventListener('mousedown',(e) =>{
-// 	currentTileId = e.target.id
-//
-// 	e.target.classList.add('tile--focus')
-// 	
-// })
-
-
-// board.addEventListener('mouseup',(e) =>{
-// 	if((currentTileId !== e.target.id) || e.target.classList.contains('tile--clicked')) return
-//
-// 	if(isInitialClick){
-// 		setup(e.target)
-// 		isInitialClick = false
-// 	}
-// 	clickTile(e.target)
-//
-// 	currentTileId = null
-// })
+let board = document.getElementById('board')
+let isEndGame = false
 
 // Функции
 function renderTile(){
-	board.style.width = `${boardTaleCountY * tileWidth}px`
+	board.style.width = `${boardSideLength * tileWidth}px`
 
-	for(let index = 0; index < tillQty; index++){
+	for(let index = 0; index < boardSize; index++){
 		createTile(index)
 	}
 }
 
-function setup(){
+function setup(initialTile){
 	let bombsLeft = bombQty
+	let numberBuff = {}
+	let bombsBuff = {}
 
 	let x = 0
 	let y = 0
@@ -67,23 +44,48 @@ function setup(){
 
 	//Вычисление позиций бомб
 	document.querySelectorAll('.tile').forEach((tile) => {
-		tile.setAttribute('data-tile', `${x},${y}`)
-		let random_boolean = Math.random() < 0.2
+		function addInNumberBuff(key) {
+			if(Object.hasOwn(numberBuff,key)){
+				numberBuff[key]++
+				return
+			}
 
-		if (random_boolean && bombsLeft) {
+			numberBuff[key] = 1
+		}
+
+		tile.setAttribute('data-tile', `${x},${y}`)
+
+		let random_boolean = Math.random() < 0.25
+
+		if (tile !== initialTile && random_boolean && bombsLeft) {
 			bombsLeft--
-			bombs.add(`${x},${y}`)
-			if (x > 0) numbers.push(`${x-1},${y}`)
-			if (x < boardTaleCountX - 1) numbers.push(`${x+1},${y}`)
-			if (y > 0) numbers.push(`${x},${y-1}`)
-			if (y < boardTaleCountX - 1) numbers.push(`${x},${y+1}`)
+			bombsBuff[`${x},${y}`] = true
+
+			if (x > 0) {
+				addInNumberBuff(`${x-1},${y}`)
+			}		
+			if (x < boardSize - 1)	{
+				addInNumberBuff(`${x+1},${y}`)
+			}		
+			if (y > 0){
+				addInNumberBuff(`${x},${y-1}`)
+			} 
+			if (y < boardSideLength - 1) {
+				addInNumberBuff(`${x},${y+1}`)
+			}
 			
-			if (x > 0 && y > 0) numbers.push(`${x-1},${y-1}`)
-			if (x < boardTaleCountX - 1 && y < boardTaleCountX - 1) numbers.push(`${x+1},${y+1}`)
-			
-			if (y > 0 && x < boardTaleCountX - 1) numbers.push(`${x+1},${y-1}`)
-			if (x > 0 && y < boardTaleCountX - 1) numbers.push(`${x-1},${y+1}`)
-			
+			if (x > 0 && y > 0) {
+				addInNumberBuff(`${x-1},${y-1}`)
+			}
+			if (x < boardSideLength - 1 && y < boardSideLength - 1){
+				addInNumberBuff(`${x+1},${y+1}`)
+			} 
+			if (y > 0 && x < boardSideLength - 1) 	{
+				addInNumberBuff(`${x+1},${y-1}`)
+			}
+			if (x > 0 && y < boardSideLength - 1){
+				addInNumberBuff(`${x-1},${y+1}`)
+			} 
 		}
 
 		x++
@@ -92,20 +94,14 @@ function setup(){
 			x = 0
 			y++
 		}
-
-
-
 	})
 
-	// TODO: Поменять на внутреннее хранилище вместо записи в data-num
-	numbers.forEach(num => {
-		let coords = num.split(',')
-		let tile = document.querySelectorAll(`[data-tile="${parseInt(coords[0])},${parseInt(coords[1])}"]`)[0]
-		let dataNum = parseInt(tile.getAttribute('data-num'))
-		if (!dataNum) dataNum = 0
-		tile.setAttribute('data-num', dataNum + 1)
-	})
-
+	Object.entries(numberBuff).forEach(([key,value]) => {
+		tilesState.set(key,{
+			count: value,
+			...(Object.hasOwn(bombsBuff,key) && {hasBomb: true}),
+		}) 
+	}) 
 }
 
 	
@@ -115,27 +111,25 @@ function setup(){
 	 * @param {DomElement} tale - Обрабатываемая ячейка
 	 */
 function clickTile(tile){
-	console.log(tile)
 	if (tile.classList.contains('tile--checked') || tile.classList.contains('tile--flagged')) return
+
 	let coordinate = tile.getAttribute('data-tile')
-	if (bombs.has(coordinate)) {
-		// endGame(tile)
-		alert('Ты проиграл')
-		return
-	} else {
-		
-		let num = tile.getAttribute('data-num')
-		if (num != null) {
+	
+	if(tilesState.has(coordinate)){
+		if (tilesState.get(coordinate).hasBomb) {
+			endGame(tile)
+		} else {
+			let num = tilesState.get(coordinate).count
 			tile.classList.add('tile--checked')
 			tile.classList.add(`tile${tileMap.get(+num)}`)
 			setTimeout(() => {
 				// checkVictory()
 			}, 100)
-			return
 		}
-		
-		checkTile(coordinate)
+		return
 	}
+
+	checkTile(coordinate)
 	tile.classList.add('tile--checked')
 }
 
@@ -155,7 +149,7 @@ function checkTile(coordinate){
 			let targetW = document.querySelectorAll(`[data-tile="${x-1},${y}"`)[0]
 			clickTile(targetW, `${x-1},${y}`)
 		}
-		if (x < 16 - 1) {
+		if (x < boardSideLength - 1) {
 			let targetE = document.querySelectorAll(`[data-tile="${x+1},${y}"`)[0]
 			clickTile(targetE, `${x+1},${y}`)
 		}
@@ -163,7 +157,7 @@ function checkTile(coordinate){
 			let targetN = document.querySelectorAll(`[data-tile="${x},${y-1}"]`)[0]
 			clickTile(targetN, `${x},${y-1}`)
 		}
-		if (y < 16 - 1) {
+		if (y < boardSideLength - 1) {
 			let targetS = document.querySelectorAll(`[data-tile="${x},${y+1}"]`)[0]
 			clickTile(targetS, `${x},${y+1}`)
 		}
@@ -172,16 +166,16 @@ function checkTile(coordinate){
 			let targetNW = document.querySelectorAll(`[data-tile="${x-1},${y-1}"`)[0]
 			clickTile(targetNW, `${x-1},${y-1}`)
 		}
-		if (x < 16 - 1 && y < 16 - 1) {
+		if (x < 16 - 1 && y < boardSideLength - 1) {
 			let targetSE = document.querySelectorAll(`[data-tile="${x+1},${y+1}"`)[0]
 			clickTile(targetSE, `${x+1},${y+1}`)
 		}
 		
-		if (y > 0 && x < 16 - 1) {
+		if (y > 0 && x < boardSideLength - 1) {
 			let targetNE = document.querySelectorAll(`[data-tile="${x+1},${y-1}"]`)[0]
 			clickTile(targetNE, `${x+1},${y-1}`)
 		}
-		if (x > 0 && y < 16 - 1) {
+		if (x > 0 && y < boardSideLength - 1) {
 			let targetSW = document.querySelectorAll(`[data-tile="${x-1},${y+1}"`)[0]
 			clickTile(targetSW, `${x-1},${y+1}`)
 		}
@@ -190,29 +184,53 @@ function checkTile(coordinate){
 
 
 function createTile(indexId){
+	function checkEndGame (){
+		return new Promise((resolve) => {
+			if(!isEndGame) resolve()
+		})
+	}
+	
 	let tile = document.createElement('div')
 	tile.classList.add('tile')
-	// till.id = `tile_${indexId}`
 
 	tile.addEventListener('mousedown', ()=> {
-		tile.classList.add('tile--focus')
+		checkEndGame()
+			.then(()=>{
+				tile.classList.add('tile--focus')
+			})
 	})
 		
 	tile.addEventListener('mouseout', ()=> {
-		tile.classList.remove('tile--focus')
+		checkEndGame()
+			.then(()=>{
+				tile.classList.remove('tile--focus')
+			})
 	})
 
 	tile.addEventListener('click', function(e) {
-		if(isInitialClick){
-			setup(e.target)
-			isInitialClick = false
-		}
-		clickTile(tile)
-
+		checkEndGame()
+			.then(()=> {
+				if(isInitialClick){
+					setup(e.target)
+					isInitialClick = false
+				}
+				clickTile(tile)
+			})
 	})
 
 	board.appendChild(tile)
 }
 
+
+function endGame(tile){
+	isEndGame = true	
+
+	tile.classList.add('tile-bomb--focus')
+
+	tilesState.forEach((value,key) =>{
+		if(!Object.hasOwn(value,'hasBomb')) return
+		document.querySelector(`.tile[data-tile="${key}"]`).classList.add('tile-bomb')
+	})
+}
 
 renderTile()
